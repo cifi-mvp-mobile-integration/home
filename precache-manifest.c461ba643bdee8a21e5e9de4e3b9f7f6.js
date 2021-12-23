2fc817a53a8775871d6a54e6a2272f46e3f6583e
Tricky4 <64266145+Tricky4@users.noreply.github.com>
2021-12-23 13:16:43 +0530

    Updates

---
+++ precache-manifest.c461ba643bdee8a21e5e9de4e3b9f7f6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "252a9291a459fd52e744",
+    "url": "/home/static/js/main.252a9291.chunk.js"
+  },
+  {
+    "revision": "4e396c535ca815cd7604",
+    "url": "/home/static/js/1.4e396c53.chunk.js"
+  },
+  {
+    "revision": "e258ea1747dec9816919",
+    "url": "/home/static/js/runtime~main.e258ea17.js"
+  },
+  {
+    "revision": "6c6f52b09af2c84c6b4f2f7ce488b645",
+    "url": "/home/static/media/algo.6c6f52b0.svg"
+  },
+  {
+    "revision": "b3cf0a30824b98bc79a39e9f78c43d06",
+    "url": "/home/index.html"
+  }
+];
